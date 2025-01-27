6adbe554280d03b30414369a4a6606c3279b7732
enwask <enwask@ucf.edu>
2025-01-27 13:34:33 -0500

    add (probably broken) search endpoint

---
+++ api/search_contacts.php
@@ -0,0 +1,66 @@
+<?php
+// Start/resume session (access session data) 
+session_start();
+include 'db.php';
+
+// Connection error check
+if ($conn->connect_error) {
+    searchError("Failed to connect to database");
+    exit();
+}
+
+// Check that the session has a user ID
+if ($_SESSION["ID"] === null) {
+    searchError("Invalid session");
+    exit();
+}
+$id = $_SESSION["ID"];
+
+// Check if form is sumbmitted 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Build SQL statement from the query
+    $query = "%" . $_POST['query'] . "%";
+
+    $stmt = $conn->prepare("SELECT ID, FirstName, LastName, Phone, Email
+        FROM Contacts
+        WHERE UserId = ?
+        AND (FirstName LIKE ? OR LastName LIKE ?)");
+    // Bind user ID and query (with wildcards on both sides)
+    $stmt->bind_param("sss", $id, $query, $query);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $arr = array();
+    while ($row = $result->fetch_assoc()) {
+        array_push($arr, array(
+            "id" => $row['ID'],
+            "firstName" => $row['FirstName'],
+            "lastName" => $row['LastName'],
+            "phone" => $row['Phone'],
+            "email" => $row['Email']
+        ));
+    }
+
+    sendSearchResults($arr);
+    $stmt->close();
+} else {
+    searchError("Invalid request");
+    exit();
+}
+
+// Close the SQL connection
+$conn->close();
+
+function sendSearchResults($arr)
+{
+    header('Content-type: application/json');
+    echo '{"count": ' . count($arr) . ', "contacts": [' . json_encode($arr) . '], "error": ""}';
+}
+
+function searchError($msg)
+{
+    header('Content-type: application/json');
+    echo '{"count": 0, "contacts": [], "error": "' . $msg . '"}';
+}
+
+?>
