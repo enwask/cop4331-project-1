20c2f119a34c79f60a0babc4094c85f32593b027
zahiredu <za993782@ucf.edu>
2025-01-30 02:46:49 -0500

    Update login.php

--- api/login.php
+++ api/login.php
@@ -57,7 +57,7 @@ $conn->close();
 
 function loginSuccess($id, $firstName, $lastName)
 {
-    //header("Location: https://contactcircle.xyz/home.html");
+    header("Location: http://contactcircle.xyz/home.html");
     header('Content-type: application/json');
     echo '{"status": true, "id": ' . $id . ',"firstName": "' . $firstName . '", "lastName": "' . $lastName . '", "error": ""}';
 }
