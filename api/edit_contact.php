45b17754438d3900392123f7083c7369b48f8224
starlight426 <147207252+starlight426@users.noreply.github.com>
2025-02-02 14:04:13 -0500

    remaining endpoint placeholders

---
+++ api/edit_contact.php
@@ -0,0 +1,68 @@
+<?php
+// Start/resume session (access session data) 
+session_start();
+include 'db.php';
+
+// Connection error check
+if ($conn->connect_error) {
+    searchError("Failed to connect to database");
+    exit();
+}
+
+// Check that the session has a user ID
+if ($_SESSION["ID"] === null) {
+    searchError("Invalid session");
+    exit();
+}
+$id = $_SESSION["ID"];
+
+// Check if form is sumbmitted 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Accept POST body as JSON
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+    // Validate email
+    if (!preg_match('/\w+/', $Login)) {
+        registerError('Invalid login');
+        exit();
+    }
+    //Validate phone
+    if (!preg_match('/^[0-9\-\s\(\)]+$/', $phone) && !empty($phone)) {
+        createContactError("Invalid phone number format");
+        exit();
+    }
+    //Validate names
+    if ($FirstName === '') {
+        createContactError('First name cannot be empty');
+        exit();
+    }
+    if ($LastName === '') {
+        createContactError('Last name cannot be empty');
+        exit();
+    }
+
+    //Check whether a user exists with this id in this contact list?
+    
+    //either update the contact info or delete the contact and insert new one
+    
+
+
+    
+    $stmt->close();
+} else {
+    createContactError("Invalid request");
+    exit();
+}
+
+// Close the SQL connection
+$conn->close();
+
+
+
+function createContactError($msg)
+{
+    header('Content-type: application/json');
+    echo '{"status": false, "count": 0, "contacts": [], "error": "' . $msg . '"}';
+}
+
+?>
