df15e13b96c9f3e02359aa5c7e896e73532b3aa8
enwask <enwask@ucf.edu>
2025-02-02 15:07:06 -0500

    add edit_contact

--- api/edit_contact.php
+++ api/edit_contact.php
@@ -5,13 +5,13 @@ include 'db.php';
 
 // Connection error check
 if ($conn->connect_error) {
-    searchError("Failed to connect to database");
+    editContactError("Failed to connect to database");
     exit();
 }
 
 // Check that the session has a user ID
 if ($_SESSION["ID"] === null) {
-    searchError("Invalid session");
+    editContactError("Invalid session");
     exit();
 }
 $id = $_SESSION["ID"];
@@ -21,36 +21,79 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Accept POST body as JSON
     $_POST = json_decode(file_get_contents('php://input'), true);
 
-    // Validate email
-    if (!preg_match('/\w+/', $Login)) {
-        registerError('Invalid login');
+    $ContactID = (int) $_POST['ID'];
+
+    // Make sure contact id is an integer
+    if ($ContactID < 1) {
+        editContactError('Invalid contact ID');
         exit();
     }
-    //Validate phone
-    if (!preg_match('/^[0-9\-\s\(\)]+$/', $phone) && !empty($phone)) {
+
+    $FirstName = $_POST['FirstName'];
+    $LastName = $_POST['LastName'];
+    $Phone = $_POST['Phone'];
+    $Email = $_POST['Email'];
+
+    // Set any empty strings to null (don't update them)
+    if ($FirstName === '') {
+        $FirstName = null;
+    }
+    if ($LastName === '') {
+        $LastName = null;
+    }
+
+    // Validate phone
+    if ($Phone === '') {
+        $Phone = null;
+    }
+    if ($Phone !== null && !preg_match('/^[0-9\-\s\(\)]+$/', $Phone)) {
         createContactError("Invalid phone number format");
         exit();
     }
-    //Validate names
-    if ($FirstName === '') {
-        createContactError('First name cannot be empty');
+
+    // Validate email
+    if ($Email === '') {
+        $Email = null;
+    }
+    if ($Email !== null && !preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $Email)) {
+        createContactError("Invalid email format");
         exit();
     }
-    if ($LastName === '') {
-        createContactError('Last name cannot be empty');
+
+    // Check if user already has a contact with the same first and last name
+    $stmt_check = $conn->prepare("SELECT *
+        FROM Contacts
+        WHERE UserID = ?
+        AND ID = ?");
+    $stmt_check->bind_param("si", $id, $ContactID);
+    $stmt_check->execute();
+    $result_check = $stmt_check->get_result();
+    if ($result_check->num_rows < 1) {
+        editContactError("No contact with this ID exists");
         exit();
     }
 
-    //Check whether a user exists with this id in this contact list?
-    
-    //either update the contact info or delete the contact and insert new one
-    
+    // Build a query for the fields that we're updating
+    $stmt = $conn->prepare("UPDATE Contacts
+        SET FirstName = COALESCE(?, FirstName),
+            LastName = COALESCE(?, LastName),
+            Phone = COALESCE(?, Phone),
+            Email = COALESCE(?, Email)
+        WHERE UserID = ?
+        AND ID = ?");
+    $stmt->bind_param("sssssi", $FirstName, $LastName, $Phone, $Email, $id, $ContactID);
+    $stmt->execute();
 
+    // Check if the update was successful
+    if ($conn->affected_rows > 0) {
+        editContactSuccess();
+    } else {
+        editContactError("Failed to edit contact");
+    }
 
-    
     $stmt->close();
 } else {
-    createContactError("Invalid request");
+    editContactError("Invalid request");
     exit();
 }
 
@@ -58,11 +101,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $conn->close();
 
 
-
-function createContactError($msg)
+function editContactError($msg)
 {
     header('Content-type: application/json');
-    echo '{"status": false, "count": 0, "contacts": [], "error": "' . $msg . '"}';
+    echo '{"status": false, "error": "' . $msg . '"}';
+}
+
+function editContactSuccess()
+{
+    header('Content-type: application/json');
+    echo '{"status": true, "error": ""}';
 }
 
 ?>
